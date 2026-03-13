8c4a06558f59c78855fb5c999c0134757674608a
nodody <17660171086@163.com>
2026-03-13 12:40:18 +0800

    不对bug还在说明不是我弄出来的，我再修修试试

--- ai-service-python/main.py
+++ ai-service-python/main.py
@@ -20,7 +20,7 @@ import shutil                  # 用于清理临时文件夹
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from core.rag_vector_db import LiteratureRAG
-
+#
 rag = LiteratureRAG()
 # 加载环境变量
 load_dotenv()
